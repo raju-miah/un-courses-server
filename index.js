bfacd504a350209b1804176e3688124907c82df4
raju-miah <rajumiahpollob7@gmail.com>
2022-10-25 21:34:38 +0600

    added courses json

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Courses API Running')
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log('Courses server running on port', port);
